df6f1d46e3ea0deb705ab3db3d29afa8e48db4c9
kev-mens <54510561+kev-mens@users.noreply.github.com>
2020-08-12 14:21:47 +0000

    3rd person camera, no aim

--- Bending AI/Assets/ThirdPersonMovement.cs
+++ Bending AI/Assets/ThirdPersonMovement.cs
@@ -7,9 +7,11 @@ public class ThirdPersonMovement : MonoBehaviour
     //public CharacterController controller;
     public Rigidbody rigidbody;
     public Vector3 movement;
-
+    //public Vector3 moveDir;
+    //public Transform cam;
     public float movementSpeed = 10 ;
     public float turnSmoothTime = 0.1f;
+    public float targetAngle;
 
     void Start()
     {
@@ -21,7 +23,10 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal"); //a to -1, d to 1
         float vertical = Input.GetAxisRaw("Vertical"); //s to -1. w to 1
-        movement = new Vector3(horizontal, 0f, vertical);    //normalise to keep same magnitude in all movements        
+        movement = new Vector3(horizontal, 0f, vertical); //unit vector on xz plane
+
+        //targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //due to unity's coordinate system orientation
+        //moveDir = Quaternion.Euler(0f,targetAngle, 0f)*Vector3.forward;        
     }
 
     void FixedUpdate()
@@ -31,7 +36,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void moveCharacter(Vector3 direction)
     {
-        rigidbody.MovePosition(transform.position + (direction * movementSpeed * Time.deltaTime));
+        rigidbody.MovePosition(transform.position + (direction * movementSpeed * Time.deltaTime)); //moves to given position
         rigidbody.angularVelocity = Vector3.zero; ///prevent rotation from environment
     }
     /*if (movement.magnitude >= 0.1f)
